630f3126abbbae152acbe43658524def05690778
Amit Kumar <amity1401@gmail.com>
2022-10-31 20:54:32 +0530

    Updated- material theme, color, minsdk and status bar color in res directory

--- app/src/main/java/com/rabarka/rootchecker/ui/theme/Color.kt
+++ app/src/main/java/com/rabarka/rootchecker/ui/theme/Color.kt
@@ -2,7 +2,25 @@ package com.rabarka.rootchecker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val md_theme_light_primary = Color(0xFF006A60)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xFF8F4D00)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFAFDFB)
+val md_theme_light_onBackground = Color(0xFF191C1B)
+val md_theme_light_surface = Color(0xFFFAFDFB)
+val md_theme_light_onSurface = Color(0xFF191C1B)
+
+
+val md_theme_dark_primary = Color(0xFF54DBC9)
+val md_theme_dark_onPrimary = Color(0xFF003731)
+val md_theme_dark_secondary = Color(0xFFFFB77B)
+val md_theme_dark_onSecondary = Color(0xFF4D2700)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_background = Color(0xFF191C1B)
+val md_theme_dark_onBackground = Color(0xFFE0E3E1)
+val md_theme_dark_surface = Color(0xFF191C1B)
+val md_theme_dark_onSurface = Color(0xFFE0E3E1)
